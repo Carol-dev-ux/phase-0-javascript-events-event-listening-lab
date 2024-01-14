f8699d5e177ccb5b68a0c6ff847bbdc4052f2e53
Caroline Wangari <karehwachira@gmail.com>
2024-01-14 20:04:13 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
-function addingEventListener() {
+function addingEventListener() { alert('I was clicked!');
 }
+const input = document.getElementById('input');
+input.addEventListener('click', addingEventListener);
